457dedbe67a92d127c60f868201002a1ff968157
HoangDSR <nvh.02021995@gmail.com>
2023-06-05 13:43:12 +0700

    Init

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:mobile/widgets/Bai1.dart';
-import 'package:mobile/widgets/bai2.dart';
-import 'package:mobile/widgets/bai3.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,28 +27,8 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    const String title = "Lab 1";
-    return SafeArea(
-      child: DefaultTabController(
-        length: 3,
-        child: Scaffold(
-          appBar: AppBar(
-            title: const Center(child: Text(title)),
-            bottom: const TabBar(
-              tabs: [
-                Tab(icon: Text('TabController')),
-                Tab(icon: Text('Web Images')),
-                Tab(icon: Text('Fade in images')),
-              ],
-            ),
-          ),
-          body: const TabBarView(children: [
-            bai1(),
-            bai2(),
-            bai3(),
-          ]),
-        ),
-      ),
+    return const SafeArea(
+      child: Center(),
     );
   }
 }

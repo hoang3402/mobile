6993689e668f83e14740fada2df136c045237710
HoangDSR <nvh.02021995@gmail.com>
2023-06-08 14:51:01 +0700

    save lab 5

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Lab 1 Work with Tabs and Display images from the internet',
+      title: 'Lab 5 Drag a UI element',
       home: MyHomePage(),
       debugShowCheckedModeBanner: false,
     );
---
+++ lib/widget/bai1.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Bai1 extends StatefulWidget {
+  const Bai1({super.key});
+
+  @override
+  State<Bai1> createState() => _Bai1State();
+}
+
+class _Bai1State extends State<Bai1> with TickerProviderStateMixin {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
